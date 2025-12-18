95bd911127b946c7ef3a161ec50dff28fb5c7eca
myous <m.yousef.r84@gmail.com>
2025-12-18 10:47:24 +0330

    fix admin.py problem

--- questionnaires/admin.py
+++ questionnaires/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from questionnaires.models import Questionnaire, Question, Option
+from questionnaires.models import Questionnaire
 
 
 # Register your models here.
@@ -10,11 +10,11 @@ from questionnaires.models import Questionnaire, Question, Option
 class QuestionnaireAdmin(admin.ModelAdmin):
     list_display = ['user_id','created_at']
 
-@admin.register(Question)
-class QuestionAdmin(admin.ModelAdmin):
-    list_display = ['name']
-
-
-@admin.register(Option)
-class OptionAdmin(admin.ModelAdmin):
-    list_display = ['name','value']
+# @admin.register(Question)
+# class QuestionAdmin(admin.ModelAdmin):
+#     list_display = ['name']
+#
+#
+# @admin.register(Option)
+# class OptionAdmin(admin.ModelAdmin):
+#     list_display = ['name','value']
